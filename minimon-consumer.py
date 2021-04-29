b1f2a2f2a014c0597ce64d1ac979610869c4cf02
Jeremy Busk <jeremybusk@gmail.com>
2021-04-28 21:43:01 -0600

    start consume file

--- minimon-consumer.py
+++ minimon-consumer.py
@@ -5,20 +5,3 @@ pgconn = psycopg2.connect(host=conf['postgres.host'],
                           dbname=conf['postgres.dbname'],
                           user=conf['postgres.dbuser'],
                           password=conf['postgres.dbpass'])
-
-def put_event(msg):
-    cur = pgconn.cursor()
-        # sql = "INSERT INTO event (msg) VALUES (%s)" % msg 
-        sql = f"INSERT INTO event (msg) VALUES ({msg})" 
-    	cur.execute(sql)
-    	# rows = cur.fetchall()
-    	pgconn.commit()
-
-topic = "quickstart-events"
-# consumer = KafkaConsumer('my_favorite_topic')
-consumer = KafkaConsumer(topic)
-
-for msg in consumer:
-    print(msg)
-
-
