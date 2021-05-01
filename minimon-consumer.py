b51b4dc59d2cafd574970ab52740e4ca79450810
Jeremy Busk <jeremybusk@gmail.com>
2021-04-30 22:14:17 -0600

    lint consumer.py

--- minimon-consumer.py
+++ minimon-consumer.py
@@ -1,7 +1,21 @@
 #!/usr/bin/env python3
-from kafka import KafkaConsumer
 
-pgconn = psycopg2.connect(host=conf['postgres.host'],
-                          dbname=conf['postgres.dbname'],
-                          user=conf['postgres.dbuser'],
-                          password=conf['postgres.dbpass'])
+from envyaml import EnvYAML
+from kafka import KafkaConsumer
+import psycopg2
+
+conf = EnvYAML('conf.yml')
+try:
+    pgconn = psycopg2.connect(host=conf['postgres.host'],
+                              dbname=conf['postgres.dbname'],
+                              user=conf['postgres.dbuser'],
+                              password=conf['postgres.dbpass'])
+    pgconn.autocommit = True
+except Exception as e:
+    print(f"E: Unable to connect to database! {e}")
+topic = conf['kafka.topic']
+bootstrap_servers = conf['kafka.bootstrap_servers']
+consumer = KafkaConsumer(topic,
+                         auto_offset_reset='latest',
+                         bootstrap_servers=bootstrap_servers,
+                         group_id=conf['kafka.group_id'])
